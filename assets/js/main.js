517c0133b62be5e5efc5bd50b6d7d8586ded8728
Jonathan Dussot <jonathandussot@gmail.com>
2025-03-25 15:52:30 +0100

    Add code for emailjs function to main.js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,14 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
